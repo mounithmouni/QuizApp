e1f61dc09b888a8c960a76ace09d66d60cd05d12
Mounith M <125748918+mounithmouni@users.noreply.github.com>
2025-03-11 11:56:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+// import DateCounter from "./DateCounter";
+
+export default function App() {
+  return (
+    <>
+      {/* <DateCounter /> */}
+
+      <div className="app">
+        <Header />
+      </div>
+    </>
+  );
+}
